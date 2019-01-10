693f86cf64acf586427189fa6b39c6d12dfb57ab
ys <sim22@nate.com>
2019-01-10 18:07:36 +0900

    todo-app-es6 add

---
+++ todo-app-es6/src/components/TodoFooter.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="clearAllContainer">
+    <span class="clearBtn" v-on:click="clearTodo">Clear All</span>
+  </div>
+</template>
+
+<script>
+export default {
+  methods:{
+    clearTodo:function(){
+      this.$emit('clearAll');
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .clearAllContainer{
+    width:8.5rem;
+    height:50px;
+    line-height:50px;
+    background-color:#fff;
+    border-radius:5px;
+    margin:0 auto;
+  }
+  .clearBtn{
+    color:#e20303;
+    display:block;
+  }
+</style>
