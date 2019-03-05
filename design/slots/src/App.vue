a5a0a34976e49e8fe502fb246eb521a4191df897
Youngsim Cha <chayoungsim@gmail.com>
2019-03-05 15:11:47 +0900

    office up

---
+++ design/slots/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <ul>
+      <item v-for="item in items" :propsdata="item"></item>
+    </ul>
+    <check-box v-model="checked"></check-box>
+  </div>
+</template>
+
+<script>
+import Item from './Item.vue';
+import CheckBox from './CheckBox.vue'
+
+export default {
+  components:{
+    Item,
+    CheckBox,
+  },
+  data(){
+    return{
+      items:['아이템1','아이템2','아이템3','아이템4','아이템5'],
+      checked:false,
+    }
+    
+  }
+}
+</script>
+
+<style>
+
+</style>
